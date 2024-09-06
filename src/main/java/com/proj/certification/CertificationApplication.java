ac5ef53044a59be1c226eddb49683c13d4781858
CletosMarcos <cleosmarcos@gmail.com>
2024-09-06 03:57:13 +0200

    first commit

---
+++ src/main/java/com/proj/certification/CertificationApplication.java
@@ -0,0 +1,14 @@
+package com.proj.certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationApplication.class, args);
+		System.out.println("Let's start");
+	}
+
+}
