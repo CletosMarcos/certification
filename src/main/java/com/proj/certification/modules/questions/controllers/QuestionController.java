1ffc21a0fdc20083d41cbf02a0849f2666128832
CletosMarcos <cleosmarcos@gmail.com>
2024-09-16 01:56:08 +0200

    add repository and controller to questions to allow GETting questions related to a given Technology

---
+++ src/main/java/com/proj/certification/modules/questions/controllers/QuestionController.java
@@ -0,0 +1,24 @@
+package com.proj.certification.modules.questions.controllers;
+
+import com.proj.certification.modules.questions.entities.QuestionEntity;
+import com.proj.certification.modules.questions.repositories.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @GetMapping("/technology/{technology}")
+    public List<QuestionEntity> findByTechnology(@PathVariable String technology) {
+        return this.questionRepository.findByTechnology(technology);
+    }
+}
