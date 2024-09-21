12f7f721a4a32e7b6adf52b57c1f8bfba66c5997
CletosMarcos <cleosmarcos@gmail.com>
2024-09-21 03:47:38 +0200

    fix "can not resolve import ..." issues

--- src/main/java/com/proj/certification/modules/students/useCases/StudentCertificationAnswersUseCase.java
+++ src/main/java/com/proj/certification/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -4,10 +4,10 @@ import com.proj.certification.modules.questions.dto.StudentCertificationAnswersD
 import com.proj.certification.modules.questions.entities.QuestionEntity;
 import com.proj.certification.modules.questions.repositories.QuestionRepository;
 import com.proj.certification.modules.students.entities.AnswersCertificationsEntity;
+import com.proj.certification.modules.students.entities.CertificationStudentEntity;
 import com.proj.certification.modules.students.entities.StudentEntity;
 import com.proj.certification.modules.students.repositories.CertificationStudentRepository;
 import com.proj.certification.modules.students.repositories.StudentRepository;
-import com.proj.certification.modules.students.entities.CertificationStudentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
